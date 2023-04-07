aa7554257fbc50a6f49d613c23ac31bc6d059feb
arata234 <90789032+arata234@users.noreply.github.com>
2023-04-07 11:34:58 +0900

    Update main.py

--- flash_card/main.py
+++ flash_card/main.py
@@ -1,55 +1,77 @@
 from tkinter import *
 import pandas as pd
+import random
+import time
+import os
 
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
-IS_ENGLISH = True
+THINKING_TIME = 3
 
-word_df = pd.read_csv("C:/Users/arata/OneDrive/ドキュメント/GitHub/Applications_for_practice/flash_card/data/french_words.csv")
-word_dict = word_df.to_dict()
-print(word_dict)
+try:
+    data = pd.read_csv("./flash_card/data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("./flash_card/data/french_words.csv")
+    word_dict = original_data.to_dict(orient="records")
+else:
+    word_dict = data.to_dict(orient="records")
 
-def change_words():
-    pass
+current_word = {}
 
+
+def next_words():
+    global current_word, after_id
+    window.after_cancel(after_id)
+    current_word = random.choice(word_dict)
+    
+    canvas.itemconfig(canvas_image, image=front_image)
+    canvas.itemconfig(title_text, text="French", fill="black")
+    canvas.itemconfig(word_text, text=current_word["French"], fill="black")
+    
+    after_id = window.after(3000, display_answer)
+    
+def display_answer():
+    global current_word
+    canvas.itemconfig(canvas_image, image=back_image)
+    canvas.itemconfig(title_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_word["English"], fill="white")
+
+def is_known():
+    word_dict.remove(current_word)
+    data = pd.DataFrame(word_dict)
+    data.to_csv("./flash_card/data/words_to_learn.csv", index=False)
+    if data.empty:
+        os.remove("./flash_card/data/words_to_learn.csv")
+        window.destroy()
+        return
+    next_words()
 
 if __name__ == "__main__":
     window = Tk()
     window.title("flash card")
     window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+    after_id = window.after(3000, func=display_answer)
     
-    # back_image = PhotoImage(file="./flash_card/images/card_back.png")
-    # bg_canvas = Canvas(width=900, height=528, bg=BACKGROUND_COLOR, highlightthickness=0)
-    # bg_canvas.create_image(400, 264, image=back_image)
-    # bg_canvas.grid(row=0, column=0, columnspan=2, rowspan=2)
-    
+    back_image = PhotoImage(file="./flash_card/images/card_back.png")
     front_image = PhotoImage(file="./flash_card/images/card_front.png")
-    ft_canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR, highlightthickness=0)
-    ft_canvas.create_image(400, 264, image=front_image)
-    ft_canvas.grid(row=0, column=0, columnspan=2)
+    canvas = Canvas(width=800, height=528, bg=BACKGROUND_COLOR, highlightthickness=0)
+    canvas_image = canvas.create_image(400, 264, image=front_image)
+    canvas.grid(row=0, column=0, columnspan=2)
     
-    #Label
-    title_label = Label(text="Welcome to", font=(FONT_NAME, 40, "italic"))
-    title_label.config(bg="white")
-    title_label.place(x=400, y=150, anchor=CENTER)
-    
-    word_label = Label(text="Flash card", font=(FONT_NAME, 60, "bold"))
-    word_label.config(bg="white")
-    word_label.place(x=400, y=280, anchor=CENTER)
-    
-    
+    title_text = canvas.create_text(400, 150, text="Welcome to", font=(FONT_NAME, 40, "italic"))
+    word_text = canvas.create_text(400, 280, text="Flash card", font=(FONT_NAME, 60, "bold"))
+
     
     # Button
     right_image = PhotoImage(file="./flash_card/images/right.png")
-    right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief=FLAT)
+    right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief=FLAT, command=is_known)
     right_button.grid(row=1, column=0)
     
     wrong_image = PhotoImage(file="./flash_card/images/wrong.png")
-    wrong_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief=FLAT)
+    wrong_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief=FLAT, command=next_words)
     wrong_button.grid(row=1, column=1)
     
-    
-    
+    next_words()
     
     window.mainloop()
