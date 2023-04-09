c39ad788aa1a669de46545b9360bf2181e2b9ca5
arata234 <aratatakamatsu@gmail.com>
2023-04-09 19:35:05 +0900

    Update wall.py

--- tetris/wall.py
+++ tetris/wall.py
@@ -1,8 +1,8 @@
 from turtle import Turtle
 
-WALL_WIDTH = 5
+WALL_WIDTH = 20
 BLOCK_SIZE = 20
-LEFT_X = -105
+LEFT_X = -90
 
 class Wall(Turtle):
     
@@ -10,22 +10,22 @@ class Wall(Turtle):
         super().__init__()
         self.create_grid()
         self.walls = []
-        for i in range(-200, 200, WALL_WIDTH):
-            self.create_wall(LEFT_X, i)
-            self.create_wall(LEFT_X+BLOCK_SIZE*10+10, i)
-        for i in range(LEFT_X, LEFT_X+BLOCK_SIZE*10+15, WALL_WIDTH):
-            self.create_wall(i, -205)
+        for i in range(-210, 210, WALL_WIDTH):
+            self.create_wall(-110, i)
+            self.create_wall(-110+BLOCK_SIZE*11, i)
+        for i in range(-90, -90+BLOCK_SIZE*10, WALL_WIDTH):
+            self.create_wall(i, -210)
         
     def create_grid(self):
         grid = Turtle("arrow")
         grid.color("gray")
         for i in range(-200, 220, 20): 
             grid.penup()
-            grid.goto(LEFT_X+WALL_WIDTH, i)
+            grid.goto(-110, i)
             grid.pendown()
-            grid.goto(LEFT_X+WALL_WIDTH+BLOCK_SIZE*10, i)
+            grid.goto(-110+BLOCK_SIZE*11, i)
         
-        for i in range(-100, 100, 20):
+        for i in range(-100, 120, 20):
             grid.penup()
             grid.goto(i, 200)
             grid.pendown()
@@ -37,7 +37,7 @@ class Wall(Turtle):
     
     def create_wall(self, x_pos, y_pos): 
         new_wall = Turtle("square")
-        new_wall.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        # new_wall.shapesize(stretch_len=0.5, stretch_wid=0.5)
         new_wall.color("white")
         new_wall.penup()
         new_wall.goto(x_pos, y_pos)
